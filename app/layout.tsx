f4900ea24aadc80abd88764b13b7911578a44c9f
CODE.RAY <mr.souvik10@gmail.com>
2023-10-11 22:07:36 +0530

    abc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '@/components/providers/theme-provider'
+
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -22,6 +24,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import { CreateServerModal } from "@/components/modals/create-server-model";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+
+export const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+        <CreateServerModal />
+        <InviteModal />
+        <EditServerModal />
+        </>
+    )
+}
