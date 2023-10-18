48c581d01af7d55892916f52337d3e9a7eb6c90b
Mohammad Ashif Mondal <91359633+Ashif6290@users.noreply.github.com>
2023-10-18 08:47:44 +0530

    Here is my code till Delete & Leave Server Modal
    I have worked on Manage Members, Channel Creation , Delete Server Modal and Leave Server Modal.

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-import { InitialModal } from "@/components/modals/initial-modals";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
