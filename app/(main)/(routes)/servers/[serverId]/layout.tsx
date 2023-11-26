3bb30b7ccd1cdddcfadc2e5c651d6685614c78a1
Abhigyan Mandal <iamabhigyan5253@gmail.com>
2023-11-26 22:37:10 +0530

    Final commit

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -6,44 +6,44 @@ import { currentProfile } from "@/lib/current-profile";
 import { ServerSidebar } from "@/components/server/server-sidebar";
 
 const ServerIdLayout = async ({
-    children,
-    params,
+  children,
+  params,
 }: {
-    children: React.ReactNode;
-    params: { serverId: string };
+  children: React.ReactNode;
+  params: { serverId: string };
 }) => {
-    const profile = await currentProfile();
+  const profile = await currentProfile();
 
-    if (!profile) {
-        return redirectToSignIn();
-    }
+  if (!profile) {
+    return redirectToSignIn();
+  }
 
-    const server = await db.server.findUnique({
-        where: {
-            id: params.serverId,
-            members: {
-                some: {
-                    profileId: profile.id
-                }
-            }
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id
         }
-    });
-
-    if (!server) {
-        return redirect("/");
+      }
     }
+  });
 
-    return (
-        <div className="h-full">
-            <div 
-            className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-              <ServerSidebar serverId={params.serverId} />
-            </div>
-            <main className="h-full md:pl-60">
-             {children}
-            </main>
-        </div>
-     );
+  if (!server) {
+    return redirect("/");
+  }
+
+  return ( 
+    <div className="h-full">
+      <div 
+      className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">
+        {children}
+      </main>
+    </div>
+   );
 }
-
+ 
 export default ServerIdLayout;
--- lib/conversation.ts
+++ lib/conversation.ts
@@ -1,66 +1,63 @@
 import { db } from "@/lib/db";
 
 export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
-    let conversation = await findConversation(memberOneId, memberTwoId) || await
-    findConversation(memberTwoId, memberOneId);
+  let conversation = await findConversation(memberOneId, memberTwoId) || await findConversation(memberTwoId, memberOneId);
 
-    if (!conversation) {
-        conversation = await createNewConversation(memberOneId, memberTwoId);
-    }
+  if (!conversation) {
+    conversation = await createNewConversation(memberOneId, memberTwoId);
+  }
 
-    return conversation;
+  return conversation;
 }
 
-const findConversation = async (memberOneId: string, memberTwoId:
-string) => {
-    try{
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
     return await db.conversation.findFirst({
-        where: {
-            AND:[
-                { memberOneId: memberOneId },
-                { memberTwoId: memberTwoId },
-            ]
+      where: {
+        AND: [
+          { memberOneId: memberOneId },
+          { memberTwoId: memberTwoId },
+        ]
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          }
         },
-        include: {
-            memberOne: {
-                include: {
-                    profile: true,
-                }
-            },
-            memberTwo: {
-                include:{
-                    profile: true,
-                }
-            }
+        memberTwo: {
+          include: {
+            profile: true,
+          }
         }
+      }
     });
-} catch {
+  } catch {
     return null;
-}
+  }
 }
 
-const createNewConversation = async (memberOneId: string,
-memberTwoId: string) => {
-    try {
-        return await db.conversation.create({
-            data: {
-                memberOneId,
-                memberTwoId
-            },
-            include: {
-                memberOne: {
-                    include: {
-                        profile: true,
-                    }
-                },
-                memberTwo: {
-                    include: {
-                        profile: true,
-                    }
-                }
-            }
-        });
-    }   catch{
-        return null;
-    }
+const createNewConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          }
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          }
+        }
+      }
+    })
+  } catch {
+    return null;
+  }
 }
